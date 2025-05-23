6bbfdeea69359c13a628cd15ea8d8403bc470028
LePtiDarron <vgarreau94@gmail.com>
2025-05-23 13:36:10 +0200

    adress

--- src/index.js
+++ src/index.js
@@ -11,4 +11,4 @@ app.use('/api/campaigns', campaignsRoutes);
 app.get('/api/', async (req, res) => {res.json({message: 'Server running'})});
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
