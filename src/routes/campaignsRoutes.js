daa352b096a88e54156f212dcab49fe4f5ceb830
LePtiDarron <vgarreau94@gmail.com>
2025-05-26 14:33:26 +0200

    [ADD] token

--- src/routes/campaignsRoutes.js
+++ src/routes/campaignsRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   const { model_id, name, lang, from_name, from_email, start_date, end_date } = req.body;
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.startsWith('Bearer ') ? authHeader.slice(7) : null;
 
   if (!model_id || !name || !Array.isArray(lang) || lang.length === 0 || !from_name || !from_email || !start_date || !end_date)
   {
     return res.status(400).json({ Error: "Missing fields" });
   }
 
+  console.log(`REQUETE DE '${token}'`);
   console.log("Campagne créée avec l'ID: ", model_id);
   console.log("Nom de la campagne   :", name);
   console.log("Langue(s)            :", lang);
