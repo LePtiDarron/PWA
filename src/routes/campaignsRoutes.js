1e60c5eb7e741c7aefa6bff543c3412f7ba86759
LePtiDarron <vgarreau94@gmail.com>
2025-05-26 10:24:11 +0200

    [FIX] logs

--- src/routes/campaignsRoutes.js
+++ src/routes/campaignsRoutes.js
@@ -10,14 +10,14 @@ router.post('/', (req, res) => {
   }
 
   console.log("Campagne créée avec l'ID: ", model_id);
-  console.log("Nom de la campagne   :", model_id);
+  console.log("Nom de la campagne   :", name);
   console.log("Langue(s)            :", lang);
   console.log("Nom                  :", from_name);
   console.log("Email                :", from_email);
   console.log("Date:                :", start_date, "-", end_date);
 
   res.status(200).json({
-    message: `Campagne créée campaign->id=1`,
+    message: `Campagne créée`,
     campaign_id: 1
   });
 });
